acda464da05de47965749579eed0bb74f11f573a
AlfinaLatifa <112874008+AlfinaLatifa@users.noreply.github.com>
2022-09-05 22:06:36 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Alfina Latifa Maysara" << endl;
+    cout << "A11.2022.14143" << endl;
+    cout << "Jalan Elangsari Barat No.2" << endl;
+    cout << "SMA NEGERI 2 MRANGGEN" << endl;
+
+    return 0;
+}
