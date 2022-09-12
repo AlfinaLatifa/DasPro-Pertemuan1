1cb3580c45590e7906cf5afd6e10a0aa526353c4
AlfinaLatifa <112874008+AlfinaLatifa@users.noreply.github.com>
2022-09-12 20:27:47 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "hai Alfina Latifa!" << endl;
+    /*
+    printf ("A11.2022.14143");
+    integer
+    king
+    queen
+    */
+
+    int u = 18;
+    float t = 159.5;
+    char b = 'M';
+    string ub = "XXL";
+
+    cout << "umur saya: " << u;
+    cout << "\nTinggi badan saya: " << t;
+    cout << "\nUkuran baju saya: " << ub;
+    return 0;
+}
