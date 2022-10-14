c5d46d0be01618806d9bbf2ab190bbfcdf7cc43d
AlfinaLatifa <112874008+AlfinaLatifa@users.noreply.github.com>
2022-10-15 05:26:29 +0700

    Add files via upload

---
+++ Tugas Pertemuan 11/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    system ("cls");
+    cout << "===================" << endl;
+    cout << "Alfina Latifa Maysara" << endl;
+    cout << "A11.2022.14143" << endl;
+    cout << "===================" << endl;
+
+    cout << "Deret Fibonacci: " << endl;
+    int x = 0, y = 1, hasil = 0;
+    for (int i = 0; i <= 9; i++)
+    {
+
+        cout << x << "+" << y << "=" << x+y << endl;
+        hasil = x + y;
+        x = y;
+        y = hasil;
+    }
+
+    return 0;
+}
